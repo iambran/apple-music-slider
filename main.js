8c65202c196488fdc5a5fc69fd174ca3b216f985
Brandon Zhang <75633537+iambran@users.noreply.github.com>
2022-04-30 14:11:47 +0800

    slider ok now

--- main.js
+++ main.js
@@ -4,22 +4,48 @@ const listItemWidth = listItems[0].clientWidth
 const gap = Number(window.getComputedStyle(list).getPropertyValue('--grid-column-gap').replace('px', '').trim())
 
 console.log(listItemWidth, gap)
-document.querySelector('.music-grid-nav__arrow--next').addEventListener('click', showNextSlide)
-document.querySelector('.music-grid-nav__arrow--previous').addEventListener('click', showPreviousSlide)
+let nextButton = document.querySelector('.music-grid-nav__arrow--next')
+nextButton.addEventListener('click', showNextSlide)
+let previousButton = document.querySelector('.music-grid-nav__arrow--previous')
+previousButton.addEventListener('click', showPreviousSlide)
 
 const totalItems = listItems.length
-const itemPerSlide = 5
-let currentSlide = 1
-const itemsHidden = () => {
-  return totalItems - itemPerSlide * currentSlide
-}
-
+const itemPerSlide = 3
 function showNextSlide() {
   list.scrollLeft += (listItemWidth + gap) * itemPerSlide
-  console.log(itemsHidden())
 }
 
 function showPreviousSlide() {
   list.scrollLeft -= (listItemWidth + gap) * itemPerSlide
-  console.log(itemsHidden())
+}
+
+let firstItem = document.querySelector('.music-grid__list-item:first-child')
+let lastItem = document.querySelector('.music-grid__list-item:last-child')
+
+let options = {
+  root: list,
+  rootMargin: '0px',
+  thredhold: 0.8
+}
+
+let observer = new IntersectionObserver(handleIntersect, options)
+observer.observe(firstItem)
+observer.observe(lastItem)
+
+function handleIntersect(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.target.previousElementSibling) {
+      if (entry.isIntersecting) {
+        previousButton.style.display = 'none'
+      } else {
+        previousButton.style.display = 'block'
+      }
+    } else {
+      if (entry.isIntersecting) {
+        nextButton.style.display = 'none'
+      } else {
+        nextButton.style.display = 'block'
+      }
+    }
+  })
 }
